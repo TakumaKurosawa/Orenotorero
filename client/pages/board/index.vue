efee142db7db378da1776d8a1b5f0028ff4097ea
KobayashiFumiaki <k18032kk@aitech.ac.jp>
2019-12-04 13:23:40 +0900

    サンプルjsonデータを作成しboard/index.vueで表示した

--- client/pages/board/index.vue
+++ client/pages/board/index.vue
@@ -1,13 +1,18 @@
 <template>
-  <h1>ボードページ</h1>
-  <!-- 
-        ここにボードコンポーネント配置 
-        -->
+  <div>
+    <h1>ボードページ</h1>
+    <h2>{{ boardData }}</h2>
+    <!--
+          ここにボードコンポーネント配置
+          -->
+  </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
 
 @Component
-export default class BoardTop extends Vue {}
+export default class BoardTop extends Vue {
+  boardData: object = this.$store.state.boardData;
+}
 </script>
