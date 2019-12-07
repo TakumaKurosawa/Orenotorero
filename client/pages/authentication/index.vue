72bfbddfb8b0eeb5678734536063ce4429f2e260
KobayashiFumiaki <k18032kk@aitech.ac.jp>
2019-12-07 19:03:07 +0900

    ログインページを作りました

--- client/pages/authentication/index.vue
+++ client/pages/authentication/index.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-    <h1>{{ boo }}</h1>
+    <h1>ログインページ</h1>
     <v-tabs color="white" centered dark>
       <v-tabs-slider color="white"></v-tabs-slider>
-      <v-tab v-on:click="indicateLogin = true">
+      <v-tab @click="indicateLogin = true">
         ログイン
       </v-tab>
-      <v-tab v-on:click="indicateLogin = false">
+      <v-tab @click="indicateLogin = false">
         新規登録
       </v-tab>
     </v-tabs>
-    <SignIn v-if="indicateLogin == true"></SignIn>
-    <SignUp v-if="indicateLogin == false"></SignUp>
+    <SignIn v-if="indicateLogin"></SignIn>
+    <SignUp v-if="!indicateLogin"></SignUp>
   </div>
 </template>
 
