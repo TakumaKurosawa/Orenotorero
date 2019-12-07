be5db757d544b849ca84c7f3fbfa2107068969db
x18067xx <x18067xx@aitech.ac.jp>
2019-12-07 18:48:06 +0900

    ボタンコンポーネントを完成させた

--- client/components/authentication/SignIn.vue
+++ client/components/authentication/SignIn.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
     <h2>ログイン</h2>
+    <myButton :is-valid="isValid" @submit="loginAction()"></myButton>
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
+import myButton from '../../components/button.vue'
 
-@Component
-export default class SignIn extends Vue {}
+@Component({
+  components: {
+    myButton
+  }
+})
+export default class SignIn extends Vue {
+  isValid = true
+  loginAction() {
+    console.log('ログインできますで')
+  }
+}
 </script>
