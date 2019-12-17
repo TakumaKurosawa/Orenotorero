e21822b483fda51dca04cc58b296a628ef4fcfe5
KobayashiFumiaki <k18032kk@aitech.ac.jp>
2019-12-17 18:33:04 +0900

    LabelとIconのコンポーネントを作った
    nuxt-property-decoratorに修正
    
    signinとsignupのボタンを修正

---
+++ client/components/atom/LabelIcon.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>
+    <v-icon>{{ iconName }}</v-icon
+    >{{ textLabel }}
+  </p>
+</template>
+
+<script lang="ts">
+import { Vue, Prop, Component } from 'nuxt-property-decorator'
+
+@Component
+export default class BaseButton extends Vue {
+  @Prop({ type: Array, required: true })
+  textLabel!: Array<string>
+
+  @Prop({ type: Array, required: false, default: '' })
+  iconName!: Array<string>
+}
+</script>
