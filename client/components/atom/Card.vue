d0db91ff3620027d58166e0b34cc98ff9efc8066
KobayashiFumiaki <k18032kk@aitech.ac.jp>
2019-12-17 19:39:54 +0900

    カードコンポーネントを作成した

---
+++ client/components/atom/Card.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-card :width="cardWidth" :height="cardHeight" :img="cardImg">
+    <v-card-title>{{ cardTitle }}</v-card-title>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { Vue, Prop, Component } from 'nuxt-property-decorator'
+
+@Component
+export default class TextField extends Vue {
+  @Prop({ type: String, required: true })
+  cardTitle!: string
+
+  @Prop({ type: String, required: true })
+  cardImg!: string
+
+  @Prop({ type: Number, required: true })
+  cardWidth!: number
+
+  @Prop({ type: Number, required: true })
+  cardHeight!: number
+}
+</script>
