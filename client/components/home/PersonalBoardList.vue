825f57471211eb4e1fa507dc6e009761423b0cbf
KobayashiFumiaki <k18032kk@aitech.ac.jp>
2019-12-18 17:32:18 +0900

    自分のボードを全て表示するコンポーネントを作成した

---
+++ client/components/home/PersonalBoardList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h2>ボードリスト</h2>
+    <LabelIcon
+      :text-label="'パーソナルボード'"
+      :icon-name="'mdi-account-outline'"
+    ></LabelIcon>
+    <Card
+      v-for="item in cardTitles"
+      :key="item.id"
+      :card-title="item.title"
+      :card-height="100"
+      :card-width="180"
+      :card-img="'https://cdn.vuetifyjs.com/images/cards/docks.jpg'"
+    ></Card>
+  </div>
+</template>
+<script lang="ts">
+import { Vue, Prop, Component } from 'nuxt-property-decorator'
+import LabelIcon from '@/components/atom/LabelIcon.vue'
+import Card from '@/components/atom/Card.vue'
+
+@Component({
+  components: {
+    LabelIcon,
+    Card
+  }
+})
+export default class BoardList extends Vue {
+  @Prop({ type: Array, required: true })
+  cardTitles!: Array<string>
+}
+</script>
--- client/pages/home/index.vue
+++ client/pages/home/index.vue
@@ -2,20 +2,31 @@
   <div>
     <h1>ユーザーTOPページ</h1>
     <MenuList></MenuList>
-    <BoardList></BoardList>
+    <PersonalBoardList :card-titles="cardTitles"></PersonalBoardList>
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'nuxt-property-decorator'
 import MenuList from '../../components/home/MenuList.vue'
-import BoardList from '../../components/home/BoardList.vue'
+import PersonalBoardList from '../../components/home/PersonalBoardList.vue'
 
 @Component({
   components: {
     MenuList,
-    BoardList
+    PersonalBoardList
   }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  cardTitles = [
+    { title: 'test1' },
+    { title: 'test2' },
+    { title: 'test3' },
+    { title: 'test4' },
+    { title: 'test5' },
+    { title: 'test6' },
+    { title: 'test7' },
+    { title: 'test8' }
+  ]
+}
 </script>
