0c44ff190ed0ef607931890fca4a149fc1f211e1
今枝佑樹 <edasan0308@edasan0308.local>
2020-01-15 12:34:10 +0900

    Many to Manyの部分を修正。おそらくこれではだめなので、要修正

--- server/API_server/src/db/Model/Board.go
+++ server/API_server/src/db/Model/Board.go
@@ -1,14 +1,17 @@
 package model
 
-import "time"
+import (
+	"time"
+)
 
 type Board struct {
-	Id            int       `json:"id"              gorm:"type:int AUTO_INCREMENT;PRIMARY_KEY;NOT NULL"`
-	CreatedUserId string    `json:"created_user_id" gorm:"type:varchar(255); NOT NULL"`
-	Title         string    `json:"title"           gorm:"type:varchar(255); NOT NULL"`
-	Publish       bool      `json:"publish"         gorm:"type:boolean; NOT NULL"`
-	BgImg         string    `json:"bg_img"          gorm:"type:varchar(255); NOT NULL"`
-	LastAccess    time.Time `json:"last_access"     gorm:"type:datetime; NOT NULL"`
-	InviteURL     time.Time `json:"invite_url"      gorm:"invite_url;type:varchar(255); NOT NULL"`
-	Kanbans       []Kanban  `json:"kanbans"			gorm:"foreignkey:BoardId"`
+	Id            int       `json:"id"              gorm:"type:int AUTO_INCREMENT;PRIMARY_KEY;NOT NULL;"`
+	CreatedUserId string    `json:"created_user_id" gorm:"type:varchar(255); NOT NULL;"`
+	Title         string    `json:"title"           gorm:"type:varchar(255); NOT NULL;"`
+	Publish       bool      `json:"publish"         gorm:"type:boolean; NOT NULL;"`
+	BgImg         string    `json:"bg_img"          gorm:"type:varchar(255); NOT NULL;"`
+	LastAccess    time.Time `json:"last_access"     gorm:"type:datetime; NOT NULL;"`
+	InviteURL     string 	`json:"invite_url"      gorm:"invite_url;type:varchar(255); NOT NULL;"`
+	Kanbans       []Kanban  `json:"kanbans"			gorm:"foreignkey:BoardId;"`
+	Users         []*User   `json:"users"			gorm:"many2many:user_boards;"`
 }
