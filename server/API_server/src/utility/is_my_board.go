a0a333fa37e346db2dd6a0412df7021e9e19b528
Takumaron <tennis.takumax@gmail.com>
2020-01-17 22:56:58 +0900

    Utilityとしてボードへの権限があるかチェックできる関数を用意した

---
+++ server/API_server/src/utility/is_my_board.go
@@ -0,0 +1,23 @@
+package utility
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	"orenotorero/db/Model"
+)
+
+func IsMyBoard(dbInstance *gorm.DB, userId string, boardId int) bool {
+	//モデル変数の宣言
+	var user model.User
+	var board model.Board
+
+	dbInstance.Where("id=?", userId).Find(&user)
+	dbInstance.Model(&user).Where("board_id=?", boardId).Related(&board, "Boards")
+	fmt.Printf("%+v\n\n", board)
+
+	if board.Id != 0 {
+		return true
+	} else {
+		return false
+	}
+}
