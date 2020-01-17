20e83a9b1126f703a99d1e1d41f03ad592a4cb58
今枝佑樹 <edasan0308@edasan0308.local>
2020-01-17 15:25:00 +0900

    不要な変数の削除を行なった

--- server/API_server/src/infrastructure/mysqlDB/board.go
+++ server/API_server/src/infrastructure/mysqlDB/board.go
@@ -57,15 +57,14 @@ func (p *BoardRepositoryImpliment) UpdateBoardPublish(userId string, boardId int
 
 	//モデル変数の宣言
 	var user model.User
-	var boards []model.Board
 
 	//userIdによるユーザー情報の取得
 	p.DB.Where("id=?", userId).Find(&user)
 
 	//取得したユーザーに対応するBoardを取得
-	p.DB.Model(&user).Related(&boards, "Boards")
+	p.DB.Model(&user).Related(&user.Boards, "Boards")
 
-	for _, board := range boards {
+	for _, board := range user.Boards {
 		//該当のBoardを見つけたらUPDATEをかける
 		if board.Id == boardId {
 			p.DB.Model(&board).Update("publish", publish)
--- server/API_server/src/repository/board.go
+++ server/API_server/src/repository/board.go
@@ -7,5 +7,5 @@ import (
 type BoardRepository interface {
 	SelectByUserId(userId string) []model.Board
 	InsertBoard(userId, title, img string) error
-	UpdateBoardPublish(id int, publish bool) error
+	UpdateBoardPublish(userId string, boardId int, publish bool) error
 }
