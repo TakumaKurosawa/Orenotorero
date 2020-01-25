caddcccb17b0ac05512fb337d18c77c702805a94
Takumaron <tennis.takumax@gmail.com>
2020-01-25 21:41:14 +0900

    動くようになったよ

--- server/API_server/src/repository/card.go
+++ server/API_server/src/repository/card.go
@@ -8,7 +8,7 @@ import (
 type CardRepository interface {
 	InsertCard(userId string, title string, kanbanId, position int) error
 	UpdateCardDeadLine(userId string, cardId int, deadline time.Time) error
-	UpdateCardTitle(id int, title string) error
+	UpdateCardTitle(userId string, id int, title string) error
 	SelectAll(kanbanId int) ([]model.Card, error)
 	DeleteCard(userId string, cardId int) error
 }
