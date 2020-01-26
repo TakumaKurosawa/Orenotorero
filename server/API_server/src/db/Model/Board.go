e295830662ef1dc5da3008bae3f5cb27cb032242
edasan0308 <edasan0568@gmail.com>
2020-01-26 12:30:05 +0900

    無駄なデータがJSONに流れないように修正

--- server/API_server/src/db/Model/Board.go
+++ server/API_server/src/db/Model/Board.go
@@ -12,6 +12,6 @@ type Board struct {
 	BgImg         string    `json:"bg_img"          gorm:"type:varchar(255); NOT NULL"`
 	LastAccess    time.Time `json:"last_access"     gorm:"type:datetime; NOT NULL"`
 	InviteURL     string    `json:"invite_url"      gorm:"invite_url;type:varchar(255); NOT NULL"`
-	Kanbans       []Kanban  `json:"kanbans"		    gorm:"foreignkey:BoardId"`
-	Authors       []*User   `json:"users"           gorm:"many2many:user_boards;"`
+	Kanbans       []Kanban  `json:"-"		        gorm:"foreignkey:BoardId"`
+	Authors       []*User   `json:"-"               gorm:"many2many:user_boards;"`
 }
