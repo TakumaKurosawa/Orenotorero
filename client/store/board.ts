cce79948bd2ba42a68b1ed82e9e0438dacb29cee
Takumaron <tennis.takumax@gmail.com>
2020-01-26 11:34:09 +0900

    fix conflict

--- client/store/board.ts
+++ client/store/board.ts
@@ -1,4 +1,4 @@
-import { Module, MutationTree, GetterTree } from 'vuex'
+import { Module, MutationTree, GetterTree, ActionTree } from 'vuex'
 import boardData from '../assets/mock/board.json'
 import { RootState } from '~/store/index'
 
@@ -40,52 +40,7 @@ const actions: ActionTree<BoardState, RootState> = {
     commit('updateBoardData', boardData)
   }
 }
-const actions: ActionTree<BoardState, RootState> = {
-  async fetchBoardData({ rootState, commit }, boardId): Promise<any> {
-    // axios -> データを取ってくる
-    const boardData: Array<object> = await this.$axios
-      .$get('/kanban', {
-        headers: {
-          Authorization: 'Bearer ' + rootState.auth.authToken,
-          BoardId: boardId
-        },
-        data: {}
-      })
-      .catch((err: any) => {
-        console.log(err)
-      })
 
-    // mutationのupdateBoardDataを呼び出す
-    commit('updateBoardData', boardData)
-  }
-}
-// const actions: ActionTree<BoardState, RootState> = {
-// //   nuxtServerInit ({ commit }, { req }) {
-// //   if (req.headers.cookie) {
-// //     const cookieData = cookieparser.parse(req.headers.cookie);
-// //     const token = cookieData.token;
-// //     const tokenAdmin = cookieData.tokenAdmin;
-// //     if (tokenAdmin) {
-// //       commit('admin/auth/loginAdmin', tokenAdmin);
-// //     }
-// //     if (token) {
-// //       commit('login', token);
-// //     }
-// //   }
-// // },
-// // async setBoardData ({ commit }): Promise<any> {
-// //   const boardData: any = this.getters.getboardData;
-// // if (Object.keys(boardData).length === 0) {
-// //   await this.$axios.get('/profile/')
-// //     .then((res) => {
-// //       console.log(res.data);
-// //       commit('setUserData', res.data);
-// //     }).catch((err) => {
-// //       console.log(err);
-// //     });
-// // }
-// }
-// };
 export const Board: Module<BoardState, RootState> = {
   namespaced: true,
   state,
