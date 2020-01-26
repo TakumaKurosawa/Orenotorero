7a25c83fd08ec04997937c4f4721ee03b7309957
x18067xx <x18067xx@aitech.ac.jp>
2020-01-26 10:54:48 +0900

    ボードデータを受け取ってカンバンとカードを表示し、ドラッグアンドドロップできるようにする

---
+++ client/components/board/Board.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <draggable v-model="boardData" class="list-group d-flex" group="kanban">
+        <kanban
+          v-for="(kanban, index) in boardData"
+          :key="index"
+          class="list-group-item mr-5"
+          :kanban-index="index"
+          :kanban="kanban"
+        ></kanban>
+      </draggable>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+import draggable from 'vuedraggable'
+import kanban from './Kanban.vue'
+
+@Component({
+  components: {
+    draggable,
+    kanban
+  }
+})
+export default class BoardCanvas extends Vue {
+  get boardData() {
+    return this.$store.getters['board/boardData']
+  }
+
+  set boardData(value: object) {
+    this.$store.commit('board/updateBoardData', value)
+  }
+}
+</script>
---
+++ client/pages/board/_id.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>ボードページ</h1>
+    <BoardHeader></BoardHeader>
+    <Board></Board>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+import draggable from 'vuedraggable'
+import Board from '~/components/board/Board.vue'
+import BoardHeader from '@/components/board/BoardHeader.vue'
+
+@Component({
+  components: {
+    draggable,
+    Board,
+    BoardHeader
+  }
+})
+export default class BoardTop extends Vue {}
+</script>
