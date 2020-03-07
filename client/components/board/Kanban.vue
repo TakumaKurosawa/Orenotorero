6192836d74cccf0639c549f99fe4f4814cdab9a5
18mac-01 <18mac-01@pc18mac-01.local>
2020-03-07 16:14:52 +0900

    カード追加関数、カード追加フォームの実装

--- client/components/board/Kanban.vue
+++ client/components/board/Kanban.vue
@@ -11,8 +11,23 @@
           :card="card"
         ></card>
       </draggable>
-      <v-card-actions>
-        <v-btn block text>カード追加</v-btn>
+      <v-card v-if="isOpenForm" raised class="ma-3">
+        <v-form>
+          <v-text-field label="カード名を入力"></v-text-field>
+        </v-form>
+      </v-card>
+      <v-card-actions v-if="isOpenForm">
+        <v-btn color="primary">カード追加</v-btn>
+        <v-btn icon @click="isOpenForm = !isOpenForm">
+          <v-icon>
+            mdi-close
+          </v-icon>
+        </v-btn>
+      </v-card-actions>
+      <v-card-actions v-else>
+        <v-btn block text @click="isOpenForm = !isOpenForm"
+          >さらにカード追加</v-btn
+        >
       </v-card-actions>
     </v-card>
   </v-col>
@@ -30,6 +45,28 @@ import Card from './Card.vue'
   }
 })
 export default class Kanban extends Vue {
+  isOpenForm: boolean = false
+  inputCardName: string = ''
+  async createCard(title: string, kanbanIndex: number, position: number) {
+    const payload = {
+      title,
+      kanban_id: kanbanIndex,
+      position
+    }
+    await this.$axios
+      .post('/card', payload, {
+        headers: {
+          Authorization: 'Bearer ' + this.$store.getters['auth/getAuthToken']
+        }
+      })
+      .then((res: any) => {
+        console.log(res.data)
+      })
+      .catch((err: any) => {
+        console.log(err)
+      })
+  }
+
   get kanbanData() {
     return this.$store.getters['board/boardData'][this.kanbanIndex].card
   }
